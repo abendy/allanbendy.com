0f778995158a6fd2b439bb703e216f66de78829d
Allan Bendy <allan.bendy@gmail.com>
2019-11-09 17:20:54 -0500

    set copyright year using js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,3 +3,7 @@ import stickybits from 'stickybits';
 
 // eslint-disable-next-line no-undef
 stickybits('#header');
+
+// set copyright year
+const year = new Date().getFullYear();
+document.getElementById('copyright').insertAdjacentHTML('beforeend', year);

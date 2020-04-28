74c9969ae2fbfd89b67329e5799913af41e638c5
Allan Bendy <allan.bendy@gmail.com>
2020-04-28 10:12:37 -0700

    removes copyright

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,3 @@ import stickybits from 'stickybits';
 
 // eslint-disable-next-line no-undef
 stickybits('#header', { useStickyClasses: true });
-
-// set copyright year
-const year = new Date().getFullYear();
-document.getElementById('copyright').insertAdjacentHTML('beforeend', year);
